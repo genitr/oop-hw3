bc826377c29fdb2f39204a38755f95482147c72d
Юрий Данильченко <genitra111@gmail.com>
2024-08-18 00:50:42 +1000

    Начальная инициализация

---
+++ src/run.py
@@ -0,0 +1,9 @@
+"""Точка входа в программу"""
+import os
+from pprint import pprint
+
+
+FILEPATH = os.path.join(os.getcwd(),'src\\data\\cookbook.txt')
+
+with open(FILEPATH, encoding='utf8') as f:
+    pass
