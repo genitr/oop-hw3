a5a2eef74619e6ed788d7485b2285b3dfbb53674
Юрий Данильченко <genitra111@gmail.com>
2024-08-18 00:52:50 +1000

    Изменения к заданию №1

--- src/run.py
+++ src/run.py
@@ -6,4 +6,16 @@ from pprint import pprint
 FILEPATH = os.path.join(os.getcwd(),'src\\data\\cookbook.txt')
 
 with open(FILEPATH, encoding='utf8') as f:
-    pass
+    cook_book = {}
+
+    for line in f:
+        recipe = line.strip()
+        icount = f.readline()
+        ilist = []
+        for _ in range(int(icount)):
+            name, amount, unit = f.readline().strip().split(' | ')
+            ilist.append({'ingredient_name': name, 'quantity': int(amount), 'measure': unit})
+        f.readline()
+        cook_book[recipe] = ilist
+
+pprint(cook_book)
